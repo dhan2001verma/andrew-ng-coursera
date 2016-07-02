bbfc3d5145968685b15c66ee7c8a0be2dc0ec809
Jayant Sharma <jayantsharma1990@gmail.com>
2016-07-03 02:51:44 +0530

    regression/logistic regression over

---
+++ machine-learning-ex2/ex2/sigmoid.m
@@ -0,0 +1,20 @@
+function g = sigmoid(z)
+%SIGMOID Compute sigmoid functoon
+%   J = SIGMOID(z) computes the sigmoid of z.
+
+% You need to return the following variables correctly 
+g = zeros(size(z));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
+%               vector or scalar).
+
+%for i=1:numel(z)
+%  g(i) = 1 / ( 1 + e^(-z(i)) );
+%end
+g = arrayfun(@(x) 1 / (1 + e^(-x)), z);
+
+
+% =============================================================
+
+end
